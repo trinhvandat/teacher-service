c837d37da715e021daf6136ba9b0598d8f69bb52
do duc anh <anh15042002hla@gmail.com>
2021-07-09 15:10:28 +0700

    build: Create RestControllerAdvice + handle TeacherServiceException

--- src/main/java/com/example/restfullapi/exception/TeacherNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.restfullapi.exception;
-
-import com.example.restfullapi.exception.common.TeacherServiceError;
-import com.example.restfullapi.exception.common.TeacherServiceException;
-
-public class TeacherNotFoundException extends TeacherServiceException {
-        public TeacherNotFoundException(){super(TeacherServiceError.TEACHER_NOT_FOUND);}
-
-}
